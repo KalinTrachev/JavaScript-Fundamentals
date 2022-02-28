1b98d9e6eb63651e6d5c00baefc476fa7ca6c11f
Kalin Trachev <kalin.trachev@gmail.com>
2022-02-28 18:02:30 +0200

    Added file lowerToUpper.js

---
+++ lowerToUpper.js
@@ -0,0 +1,11 @@
+function lowerToUpper(char) {
+
+    let toUpper = char.toUpperCase();
+
+    if (char == toUpper) {
+        console.log('upper-case');
+    } else {
+        console.log('lower-case');
+    }
+}
+lowerToUpper ('f');
